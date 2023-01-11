e7bc4892370a4ee07d1886b419278e2c4803792c
Elena <elenasaugarmayoral@gmail.com>
2023-01-11 12:40:12 +0100

    atributos añadidos

--- src/Thewicher/TheWitcher.java
+++ src/Thewicher/TheWitcher.java
@@ -2,6 +2,10 @@ package Thewicher;
 
 public class TheWitcher {
 
+	private String name;
+	private int health;
+	private boolean hasSoul;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
