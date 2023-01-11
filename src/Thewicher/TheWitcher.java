d6e2beb86f772b72ecbcb0e084d3de4b1dd169d2
Elena <elenasaugarmayoral@gmail.com>
2023-01-11 13:40:06 +0100

    método isDeath

--- src/Thewicher/TheWitcher.java
+++ src/Thewicher/TheWitcher.java
@@ -30,9 +30,29 @@ public class TheWitcher {
 	private int health;
 	private boolean hasSoul;
 	
+	public boolean isDeath() {
+		if (health==0) {
+			return true;
+		} else if (!hasSoul) {
+			return true;
+			
+		}else {
+			return false;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		TheWitcher brujo=new TheWitcher();
+		brujo.setName("Geralt de Rivia");
+		brujo.setHealth(15);
+		brujo.setHasSoul(true);
+		if (brujo.isDeath()) {
+			System.out.println(brujo.getName()+ "está muerto");
+		}else {
+			System.out.println(brujo.getName()+ "está vivo");
+		}
 		
 	}
 
