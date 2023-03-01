5f3c5c547752680b5dbe7c61d2b0bc3527f91558
Elena <elenasaugarmayoral@gmail.com>
2023-03-01 13:35:25 +0100

    generado javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Thewicher","l":"TheWitcher"}];updateSearchResults();
--- src/Thewicher/TheWitcher.java
+++ src/Thewicher/TheWitcher.java
@@ -1,35 +1,84 @@
 package Thewicher;
 
-public class TheWitcher {
+/**
+ * Clase que representa un brujo con 3 atributos, name,health y hasSoul.
+ * Esta clase se usara para un videojuego RPG.
+ * 
+ * @author Elena
+ * 
+ * @version 1.0
+ *
+ */
 
+public class TheWitcher {
+/**
+ * especificamos que es el atributo
+ * atributo name que es nombre del brujo
+ */
 	private String name;
+	//vida de brujo
+	private int health;
+	//si el brujo tiene o no alma
+	private boolean hasSoul;
+	
+	/**
+	 * Getter para obtener el nombre del brujo
+	 * @return devuelve el nombre del brujo
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * setter para introducir el nombre del brujo
+	 * @param name nombre del brujo
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * getter para obtener la cantidad de vida del brujo
+	 * @return devuelve el numero de vidas que le quedan al brujo
+	 */
 	public int getHealth() {
 		return health;
 	}
 
+	/**
+	 * setter para establecer la vida del brujo
+	 * @param health cantidad de vida del brujo
+	 */
+	
 	public void setHealth(int health) {
 		this.health = health;
 	}
 
+	/**
+	 * funcion para saber si el brujo tiene alma
+	 * @return devuelve si el brujo tiene alma o no
+	 */
 	public boolean isHasSoul() {
 		return hasSoul;
 	}
 
+	/**
+	 * setter para establecer el alma del brujo.
+	 * @param hasSoul valor bolleano, si el brujo tiene o no alma.
+	 */
 	public void setHasSoul(boolean hasSoul) {
 		this.hasSoul = hasSoul;
 	}
 
-	private int health;
-	private boolean hasSoul;
 	
+	/**
+	 * Nos dice si el brujo esta o no muerto:
+	 * <ul>
+	 * <li>si no tiene vida </li>
+	 * <li>si tiene vida pero no tiene alma </li>
+	 * </ul>
+	 * @return si esta o no muerto.
+	 */
 	public boolean isDeath() {
 		if (health==0) {
 			return true;
